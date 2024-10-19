19736018741983487a87f93dc24900395910993c
achmdabdillah <achmdabdil9@gmail.com>
2024-10-19 16:56:16 +0700

    remove comments

--- src/main/java/com/template/demo/DemoApplication.java
+++ src/main/java/com/template/demo/DemoApplication.java
@@ -2,11 +2,8 @@ package com.template.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
-// @EnableMongoRepositories(basePackages =
-// "com.template.demo.repository.MongoRepository")
 public class DemoApplication {
 
 	public static void main(String[] args) {
